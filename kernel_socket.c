0f7455e0d698a39cf594baaa951fb4130714ecd9
TheApo98 <apogioum.ag@gmail.com>
2020-12-06 16:56:06 +0200

    Implemented:  - sys_Socket(...)  - init_scb(...) (needs work)

--- kernel_socket.c
+++ kernel_socket.c
@@ -1,10 +1,37 @@
 
 #include "tinyos.h"
+#include "kernel_socket.h"
 
+file_ops socket_file_ops = {
+        .Open = NULL,
+        .Read = socket_read,
+        .Write = socket_write,
+        .Close = socket_close
+};
+
+SCB * init_scb(FCB * fcb, port_t port){
+	SCB * socket = (SCB *) xmalloc(sizeof(SCB));
+	socket->fcb = fcb;
+	socket->port = port;
+	socket->refcount = 0;
+	socket->type = SOCKET_UNBOUND;
+
+	return socket;
+}
 
 Fid_t sys_Socket(port_t port)
 {
-	return NOFILE;
+	Fid_t  fid;
+	FCB * fcb;
+	if(!FCB_reserve(1, &fid, &fcb) || (port < 0 || port >= MAX_PORT))
+		return NOFILE;
+
+	SCB * socket_cb = init_scb(fcb, port);
+
+	fcb->streamobj = socket_cb;
+	fcb->streamfunc = &socket_file_ops;
+
+	return fid;
 }
 
 int sys_Listen(Fid_t sock)
